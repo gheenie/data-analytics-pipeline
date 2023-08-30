2d8aa757ea619b414c5ee49b0c6669554d7c8879
infoomics <sbx_94@hotmail.com>
2023-08-30 17:23:35 +0100

    Number the stages

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -1,30 +1,34 @@
 import pandas
 import plotly.express as px
 
-# Requirements/hypothesis
+# 1. Requirements/hypothesis
 
-# Collecting
+# 2. Collecting/scraping
 
-# Processing
+# 3. Cleaning
+
+# 4. Processing/manipulating
 
 df = pandas.read_csv('data/unit05-data.csv')
 
+# Example 1
 df_avg_incidence_by_year = df.groupby('year', as_index=False).agg({'Estimated incidence (all forms) per 100 000 population': 'mean'})
 # print(df_avg_incidence_by_year)
 
+# Example 2
 df_singapore = df[df['country'] == 'Singapore']
 print(df_singapore)
 
-# Cleaning
+# 5a. Quantitative/statistical analysis
 
-# Quantitative/statistical analysis
+# 5b. Graphical analysis/visualisation
 
-# Graphical analysis/visualisation
-
+# Example 1
 figure = px.bar(df_avg_incidence_by_year, x='year', y='Estimated incidence (all forms) per 100 000 population')
 # figure.show()
 
+# Example 2
 figure = px.line(df_singapore, x='year', y='Estimated incidence (all forms) per 100 000 population')
 figure.show()
 
-# Decision making/conclusions
+# 6. Decision making/conclusions
