dde018470a5556215febc109283b073b39a65cb1
infoomics <sbx_94@hotmail.com>
2023-09-07 21:49:30 +0100

    Copy over world happiness report project

---
+++ README.md
@@ -0,0 +1,20 @@
+This project explores and creates some visualisations of data from the World Happiness Reports from 2015 to 2019.
+
+<br />
+
+To run this project:
+
+1. `cd` into this project's root folder
+2. Make sure you have `python 3.11.1` installed
+3. Create a virtual environment with:
+```
+python -m venv venv
+```
+4. Install dependencies with:
+```
+pip install requirements.txt
+```
+5. Run the analyses with:
+```
+python src/analysis.py
+```
--- src/data_analysis.py
+++ src/data_analysis.py
@@ -3,13 +3,16 @@ import plotly.express as px
 
 # 1. Requirements/hypothesis
 
-# 2. Collecting/scraping
+# 2. Collecting
+
+df = pandas.read_csv('data/unit05-data.csv')
+
+df_world_happiness_report = pandas.read_csv('data/2019.csv')
+df_world_happiness_report.to_csv('data/output.csv')
 
 # 3. Cleaning
 
-# 4. Processing/manipulating
-
-df = pandas.read_csv('data/unit05-data.csv')
+# 4. Processing
 
 # Example 1
 df_avg_incidence_by_year = df.groupby('year', as_index=False).agg({'Estimated incidence (all forms) per 100 000 population': 'mean'})
@@ -17,18 +20,31 @@ df_avg_incidence_by_year = df.groupby('year', as_index=False).agg({'Estimated in
 
 # Example 2
 df_singapore = df[df['country'] == 'Singapore']
-print(df_singapore)
+# print(df_singapore)
 
-# 5a. Quantitative/statistical analysis
+# 5. Quantitative analysis
 
-# 5b. Graphical analysis/visualisation
+# Which factors appear to have the greatest impact on a nation's happiness
+print(df_world_happiness_report['Score'].corr(df_world_happiness_report['GDP per capita']))
+print(df_world_happiness_report['Score'].corr(df_world_happiness_report['Healthy life expectancy']))
+print(df_world_happiness_report['Score'].corr(df_world_happiness_report['Social support']))
+print(df_world_happiness_report['Score'].corr(df_world_happiness_report['Freedom to make life choices']))
+print(df_world_happiness_report['Score'].corr(df_world_happiness_report['Perceptions of corruption']))
+print(df_world_happiness_report['Score'].corr(df_world_happiness_report['Generosity']))
+# Answers are in descending order of the above columns
 
-# Example 1
+# 6. Graphical analysis
+
+# Incidences across years
 figure = px.bar(df_avg_incidence_by_year, x='year', y='Estimated incidence (all forms) per 100 000 population')
 # figure.show()
 
-# Example 2
+# Incidences across years, within Singapore
 figure = px.line(df_singapore, x='year', y='Estimated incidence (all forms) per 100 000 population')
-figure.show()
+# figure.show()
 
-# 6. Decision making/conclusions
+# Overall happiness by country
+fig = px.bar(df, x='Score', y='Country or region', orientation='h', title='Overall happiness by country')
+# fig.show()
+
+# 7. Decision making/conclusions
